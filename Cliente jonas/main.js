a39be745319283cd0d95f1757e2f23e290be2f53
André Valeriano <129342970+andrevalerianoo@users.noreply.github.com>
2024-01-25 22:35:23 -0300

    Projeto

---
+++ Cliente jonas/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    
+
+    $('header button').click (function(){
+        $('ul').slideDown();
+    })
+
+    $('ul').on ("submit", function(e){
+        console.log("submit");
+        e.preventDefault();
+    })
+
+
+
+})
+
+
+
+
